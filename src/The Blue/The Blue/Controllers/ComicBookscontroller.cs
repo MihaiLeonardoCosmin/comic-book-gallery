fbf6c2c68a7bf726bf0a14beee1646c66ac6d17d
Dorin <dorin_mihaela@yahoo.com>
2020-03-27 13:18:35 +0200

    Updated Detail action method to use action result types

--- src/The Blue/The Blue/Controllers/ComicBookscontroller.cs
+++ src/The Blue/The Blue/Controllers/ComicBookscontroller.cs
@@ -8,9 +8,15 @@ namespace The_Blue.Controllers
 {
     public class ComicBookscontroller : Controller
     {
-        public string  Detail()
+        public ActionResult  Detail()
         {
-            return "Hello from the comic books controller!";
+            if(DateTime.Today.DayOfWeek == DayOfWeek.Friday)
+            {
+                return Redirect("/");
+            }
+            return Content("Hello from the comic books controller!");
+
+
         }
     }
 }
