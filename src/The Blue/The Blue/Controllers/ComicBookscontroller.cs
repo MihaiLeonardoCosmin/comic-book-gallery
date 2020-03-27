fb273da75f4679617c16d32c71ed5ecbcc0c71c6
Dorin <dorin_mihaela@yahoo.com>
2020-03-27 19:01:28 +0200

    Update Comic Book Detail view to be strongly typed

--- src/The Blue/The Blue/Controllers/ComicBookscontroller.cs
+++ src/The Blue/The Blue/Controllers/ComicBookscontroller.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using The_Blue.Models;
 
 namespace The_Blue.Controllers
 {
@@ -10,18 +11,22 @@ namespace The_Blue.Controllers
     {
         public ActionResult  Detail()
         {
-            ViewBag.SeriesTitle = "The Amazing Spider-Man";
-            ViewBag.IssuNumber = 700;
-            ViewBag.Description = "<p> Spider-Man este un licen care a fost muscat de un <span>paianjen la scoala </span></p>";
-            ViewBag.Artists = new string[]
+            var comicBook = new ComicBook()
             {
-                "Scripts:Dan Slot",
-                "Penciles:Humberto Ramos",
-                "Inks:Victor Olazaba",
-                "Colors:Edgar Delgado",
-                "Letters:Chris Eliopoulos"
+                SeriesTitle = "The Amazing Spider-Man",
+                IssueNumber = 700,
+                DescriptionHtml = "<p> Spider-Man este un licen care a fost muscat de un <span>paianjen la scoala </span></p>",
+                Artists = new Artist[]
+                {
+                    new Artist() {Name = "Dan Slot" , Role = "Scripts"},
+                    new Artist() {Name = "Humberto Ramos" , Role = "Penciles"},
+                    new Artist() {Name = "Victor Olazaba" , Role = "Inks"},
+                    new Artist() {Name = "Edgar Delgado" , Role = "Colors"},
+                    new Artist() {Name = "Chris Eliopoulos" , Role = "Letters"},
+                }
             };
-            return View();
+            
+            return View(comicBook);
         }
     }
 }
--- src/The Blue/The Blue/Models/ComicBook.cs
+++ src/The Blue/The Blue/Models/ComicBook.cs
@@ -11,7 +11,7 @@ namespace The_Blue.Models
         public string SeriesTitle { get; set; }
         public int IssueNumber { get; set; }
         public string DescriptionHtml { get; set; }
-        public Artist[] Artist { get; set; }
+        public Artist[] Artists { get; set; }
         public bool Favorite { get; set; }
 
         public string DisplayText
