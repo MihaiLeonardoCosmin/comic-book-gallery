3b60bdf84a165d3a9157ae8cc839949d66ae03ce
Dorin <dorin_mihaela@yahoo.com>
2020-03-27 14:29:41 +0200

    Updated Comic Book Detail view

--- src/The Blue/The Blue/Controllers/ComicBookscontroller.cs
+++ src/The Blue/The Blue/Controllers/ComicBookscontroller.cs
@@ -10,6 +10,17 @@ namespace The_Blue.Controllers
     {
         public ActionResult  Detail()
         {
+            ViewBag.SeriesTitle = "The Amazing Spider-Man";
+            ViewBag.IssuNumber = 700;
+            ViewBag.Description = "<p> Spider-Man este un licen care a fost muscat de un <span>paianjen la scoala </span></p>";
+            ViewBag.Artists = new string[]
+            {
+                "Scripts:Dan Slot",
+                "Penciles:Humberto Ramos",
+                "Inks:Victor Olazaba",
+                "Colors:Edgar Delgado",
+                "Letters:Chris Eliopoulos"
+            };
             return View();
         }
     }
