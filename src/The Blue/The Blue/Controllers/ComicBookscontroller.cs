b3a8ad0244bd57c6fcb84f9beeb26b4e43233659
Dorin <dorin_mihaela@yahoo.com>
2020-03-27 12:48:20 +0200

    Added comic books controller

---
+++ src/The Blue/The Blue/Controllers/ComicBookscontroller.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace The_Blue.Controllers
+{
+    public class ComicBookscontroller : Controller
+    {
+    }
+}
